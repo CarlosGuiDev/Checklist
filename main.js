0774983428ac61a4314223ee53720ab5a97875be
Carlos <carlosgui.541@gmail.com>
2024-02-24 18:47:17 -0300

    Updated
    Version msg, design improvement, new fonts and new color buttons / Mensagem de versão, melhoria no design, novas fontes e novas cores para os botões.

---
+++ main.js
@@ -0,0 +1,84 @@
+var enter = document.getElementById('input')
+        var button = document.getElementById('btn')
+        var clearbtn = document.getElementById('btn-clear')
+        button.addEventListener('click', clicar)
+        enter.addEventListener("keydown", tecla)
+        clearbtn.addEventListener('click', function () {
+            var divclear = document.getElementsByClassName('tarefas')[0]
+            divclear.innerHTML = ''
+        })
+
+        function clicar() {
+            var paragrafo1, newp, marcador = true
+            var txt, valor
+            txt = document.getElementById('input')
+            valor = txt.value
+            paragrafo = document.getElementsByClassName('tarefas')[0] 
+            newp = document.createElement('p')
+            newp.textContent = `- ${valor}`
+            var newp5 = newp
+            
+            if (valor > "") {
+                paragrafo.appendChild(newp5)
+                newp5.style.textAlign = "center"
+                newp5.style.cursor = "pointer"
+            }
+            
+            newp.addEventListener('click', function marcando() {
+                if (marcador) {
+                    newp5.style.textDecoration = 'line-through'
+                    marcador = false
+                } else {
+                    newp5.style.textDecoration = ''
+                    marcador = true
+                }
+            })
+
+            newp.addEventListener('dblclick', function remover(){
+                paragrafo.removeChild(newp5)
+            })
+
+            var clear = document.getElementById('input')
+            clear.value = ""
+        }
+
+        
+        
+
+        function tecla() {
+            if (event.key === "Enter") {
+                var paragrafo, newp, marcador = true
+                var txt, valor
+                txt = document.getElementById('input')
+                valor = txt.value
+                paragrafo = document.getElementsByClassName('tarefas')[0]
+                newp = document.createElement('p')
+                newp.textContent = `- ${valor}`
+                var newp5 = newp
+                if (valor > "") {
+                    paragrafo.appendChild(newp5)
+                    newp.style.textAlign = "center"
+                    newp.style.cursor = "pointer"
+                    
+                }
+
+                newp.addEventListener('click', function marcando() {
+                    
+                    if (marcador) {
+                        newp5.style.textDecoration = 'line-through'
+                        marcador = false  
+                    } else {
+                        newp5.style.textDecoration = ''
+                        marcador = true
+                    }
+                    
+                    newp.addEventListener("dblclick", function remover(){
+                        paragrafo.removeChild(newp5)
+                    })
+                })
+
+
+                var clear = document.getElementById('input')
+                clear.value = ""
+            }
+        }
